6f3bea4573611770421727e527880577cb5b6be8
cpmashir <ashircpm@gmail.com>
2026-01-21 09:55:11 +0530

    Time: 50 ms (75.81%) | Memory: 23.9 MB (69.01%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -14,16 +14,11 @@ public:
         ListNode *pA = headA;
         ListNode *pB = headB;
 
-        // If pA and pB have different lengths, they will eventually meet 
-        // after switching heads at most once.
         while (pA != pB) {
-            // If pA reaches the end, redirect to headB. Otherwise, move to next.
             pA = (pA == nullptr) ? headB : pA->next;
-            // If pB reaches the end, redirect to headA. Otherwise, move to next.
             pB = (pB == nullptr) ? headA : pB->next;
         }
 
-        // Either they meet at the intersection node, or both are nullptr (no intersection).
         return pA;
     }
 };
