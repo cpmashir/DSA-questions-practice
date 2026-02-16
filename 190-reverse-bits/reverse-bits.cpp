585b106ecf61216f1acb2bead0368e52ecd594f5
cpmashir <ashircpm@gmail.com>
2026-02-16 09:08:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (15.72%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int reverseBits(int n) {
         unsigned int res = 0;
-        unsigned int input = (unsigned int)n; // Treat as unsigned for bit logic
+        unsigned int input = (unsigned int)n;
         
         for (int i = 0; i < 32; i++) {
-            // Shift result left and add the LSB of input
             res = (res << 1) | (input & 1);
-            // Shift input right to get the next bit
             input >>= 1;
         }
         
