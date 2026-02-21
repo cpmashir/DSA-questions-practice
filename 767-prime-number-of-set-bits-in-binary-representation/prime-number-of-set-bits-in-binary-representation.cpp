8eea064b110000f3662d2979cb592e71d7cd18e5
cpmashir <ashircpm@gmail.com>
2026-02-21 21:15:52 +0530

    Time: 3 ms (72.17%) | Memory: 8.1 MB (35.12%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -2,24 +2,12 @@ class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
         int totalCount = 0;
-        
         for (int i = left; i <= right; ++i) {
-            // builtin_popcount is an efficient way to count set bits
             int bits = __builtin_popcount(i);
-            
-            // Check if bits is prime (up to 20)
-            if (isPrime(bits)) {
+            if ((665772 >> bits) & 1) {
                 totalCount++;
             }
         }
-        
         return totalCount;
-    }
-
-private:
-    bool isPrime(int n) {
-        // A simple set check is faster than a loop for small N
-        return (n == 2 || n == 3 || n == 5 || n == 7 || 
-                n == 11 || n == 13 || n == 17 || n == 19);
     }
 };
