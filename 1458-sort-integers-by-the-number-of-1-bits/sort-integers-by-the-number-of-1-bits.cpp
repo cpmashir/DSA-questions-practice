f8af89810d68e7eed992361f007a3e9582e0cd17
cpmashir <ashircpm@gmail.com>
2026-02-25 23:58:56 +0530

    Time: 1 ms (57.35%) | Memory: 14.1 MB (92.86%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -4,16 +4,13 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        // Use a lambda to define the custom sorting criteria
         std::sort(arr.begin(), arr.end(), [](const int& a, const int& b) {
             int countA = __builtin_popcount(a);
             int countB = __builtin_popcount(b);
             
-            // If bit counts are different, sort by bit count
             if (countA != countB) {
                 return countA < countB;
             }
-            // Otherwise, sort by the value of the numbers themselves
             return a < b;
         });
         
