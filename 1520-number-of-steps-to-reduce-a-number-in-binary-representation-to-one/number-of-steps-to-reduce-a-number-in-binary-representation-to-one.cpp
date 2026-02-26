498a4083e751883ad988cbd3b179b6a3a3f734e8
cpmashir <ashircpm@gmail.com>
2026-02-26 09:15:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (58.55%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -5,24 +5,15 @@ public:
         int carry = 0;
         int n = s.length();
 
-        // We process from the last character up to the second character (index 1)
-        // because the loop ends when we've reduced everything down to the first '1'.
         for (int i = n - 1; i > 0; --i) {
-            int currentBit = s[i] - '0';
-
-            if (currentBit + carry == 1) {
-                // Number is odd: Add 1 (1 step) + Divide by 2 (1 step) = 2 steps
+            if ((s[i] - '0') + carry == 1) {
                 steps += 2;
-                carry = 1; 
+                carry = 1;
             } else {
-                // Number is even: Just divide by 2 (1 step)
-                // If it was (0+0), carry stays 0. If it was (1+1), carry stays 1.
                 steps += 1;
             }
         }
 
-        // If at the end we have a carry (e.g., "1" became "10"), 
-        // we need one more division to reach "1".
         return steps + carry;
     }
 };
