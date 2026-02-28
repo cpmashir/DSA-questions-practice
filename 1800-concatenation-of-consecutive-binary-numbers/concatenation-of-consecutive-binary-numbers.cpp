638c961079e3ca148720f7583158e4ab7dba5f32
cpmashir <ashircpm@gmail.com>
2026-02-28 12:28:55 +0530

    Time: 26 ms (77.85%) | Memory: 8 MB (89.26%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -3,16 +3,9 @@ public:
     int concatenatedBinary(int n) {
         long long res = 0;
         const int MOD = 1e9 + 7;
-        
         for (int i = 1; i <= n; ++i) {
-            // __builtin_clz(i) returns the number of leading zeros in a 32-bit int.
-            // 32 - zeros = position of the highest set bit.
-            int bitLength = 32 - __builtin_clz(i);
-            
-            // Shifting and ORing is generally faster than addition
-            res = ((res << bitLength) | i) % MOD;
+            res = ((res << (32 - __builtin_clz(i))) | i) % MOD;
         }
-        
         return (int)res;
     }
 };
