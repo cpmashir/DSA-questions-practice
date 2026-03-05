8354a4187db3a5e734d1621d0bba76797dee77bd
cpmashir <ashircpm@gmail.com>
2026-03-05 13:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (20.96%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int minOperations(string s) {
         int n = s.length();
-        int count0 = 0; // Operations to match pattern "0101..."
+        int count0 = 0;
         
         for (int i = 0; i < n; i++) {
-            // In pattern "0101...", even indices should be '0', odd should be '1'
             if (i % 2 == 0) {
                 if (s[i] != '0') count0++;
             } else {
@@ -13,7 +12,6 @@ public:
             }
         }
         
-        // The cost for the other pattern ("1010...") is simply (n - count0)
         return min(count0, n - count0);
     }
 };
