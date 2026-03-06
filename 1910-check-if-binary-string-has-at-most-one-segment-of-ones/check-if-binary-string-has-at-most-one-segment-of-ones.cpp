fe88d8c8c81b5802cbb7d9ce07c51c1155c22fc1
cpmashir <ashircpm@gmail.com>
2026-03-06 10:40:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.93%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        // Since s[0] is '1', we just need to make sure 
-        // there is no '1' after the first group of '1's ends.
-        // This is equivalent to checking if "01" is a substring.
-        
-        for (int i = 1; i < s.length(); i++) {
-            if (s[i-1] == '0' && s[i] == '1') {
-                return false;
-            }
-        }
-        return true;
+        return s.find("01") == string::npos;
     }
 };
