5a0cdda935b9cbe0857316b72408d0cd1f69d4bb
cpmashir <ashircpm@gmail.com>
2026-03-09 09:02:48 +0530

    Time: 15 ms (89.43%) | Memory: 17.5 MB (52.03%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int numberOfStableArrays(int zero, int one, int limit) {
-        long long MOD = 1e9 + 7;
-        vector<vector<vector<long long>>> dp(zero + 1, vector<vector<long long>>(one + 1, vector<long long>(2, 0)));
+        const int MOD = 1e9 + 7;
+        vector<vector<int>> dp0(zero + 1, vector<int>(one + 1, 0));
+        vector<vector<int>> dp1(zero + 1, vector<int>(one + 1, 0));
 
-        for (int i = 1; i <= min(zero, limit); ++i) dp[i][0][0] = 1;
-        for (int j = 1; j <= min(one, limit); ++j) dp[0][j][1] = 1;
+        for (int i = 1; i <= min(zero, limit); ++i) dp0[i][0] = 1;
+        for (int j = 1; j <= min(one, limit); ++j) dp1[0][j] = 1;
 
         for (int i = 1; i <= zero; ++i) {
             for (int j = 1; j <= one; ++j) {
-                dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+                long long val0 = (long long)dp0[i - 1][j] + dp1[i - 1][j];
                 if (i > limit) {
-                    dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + MOD) % MOD;
+                    val0 -= dp1[i - limit - 1][j];
                 }
+                dp0[i][j] = (val0 % MOD + MOD) % MOD;
 
-                dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
+                long long val1 = (long long)dp0[i][j - 1] + dp1[i][j - 1];
                 if (j > limit) {
-                    dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + MOD) % MOD;
+                    val1 -= dp0[i][j - limit - 1];
                 }
+                dp1[i][j] = (val1 % MOD + MOD) % MOD;
             }
         }
 
-        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
+        return (dp0[zero][one] + dp1[zero][one]) % MOD;
     }
 };
