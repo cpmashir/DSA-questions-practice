5367a127259123ba8d67b3600cf8115c851fbe21
cpmashir <ashircpm@gmail.com>
2026-03-09 09:02:07 +0530

    Time: 107 ms (83.74%) | Memory: 50.9 MB (43.09%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     int numberOfStableArrays(int zero, int one, int limit) {
         long long MOD = 1e9 + 7;
-        // dp[i][j][k] -> i zeros used, j ones used, k is the last digit (0 or 1)
         vector<vector<vector<long long>>> dp(zero + 1, vector<vector<long long>>(one + 1, vector<long long>(2, 0)));
 
-        // Base cases: single sequences within the limit
         for (int i = 1; i <= min(zero, limit); ++i) dp[i][0][0] = 1;
         for (int j = 1; j <= min(one, limit); ++j) dp[0][j][1] = 1;
 
         for (int i = 1; i <= zero; ++i) {
             for (int j = 1; j <= one; ++j) {
-                // To end with a 0 at (i, j):
-                // We take the total ways to form (i-1, j) and subtract invalid sequences
                 dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
                 if (i > limit) {
-                    // Subtract sequences that would have more than 'limit' zeros
                     dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + MOD) % MOD;
                 }
 
-                // To end with a 1 at (i, j):
                 dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
                 if (j > limit) {
-                    // Subtract sequences that would have more than 'limit' ones
                     dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + MOD) % MOD;
                 }
             }
