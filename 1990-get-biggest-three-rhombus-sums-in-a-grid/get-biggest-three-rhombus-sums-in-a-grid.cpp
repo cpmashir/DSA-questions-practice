b899601779314b193dd440fdcde36f5f33e7019f
cpmashir <ashircpm@gmail.com>
2026-03-16 08:33:44 +0530

    Time: 113 ms (62.07%) | Memory: 67.2 MB (12.41%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -1,15 +1,22 @@
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> getBiggestThree(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        auto d1 = vector<vector<int>>(m + 1, vector<int>(n + 2, 0));
-        auto d2 = vector<vector<int>>(m + 1, vector<int>(n + 2, 0));
+        vector<vector<int>> upLeft(m + 1, vector<int>(n + 2, 0));
+        vector<vector<int>> upRight(m + 1, vector<int>(n + 2, 0));
         set<int> top3;
 
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                d1[i + 1][j + 1] = grid[i][j] + d1[i][j];
-                d2[i + 1][j + 1] = grid[i][j] + d2[i][j + 2];
+                upLeft[i + 1][j + 1] = grid[i][j] + upLeft[i][j];
+                upRight[i + 1][j + 1] = grid[i][j] + upRight[i][j + 2];
+                
                 top3.insert(grid[i][j]);
                 if (top3.size() > 3) top3.erase(top3.begin());
             }
@@ -18,15 +25,20 @@ public:
         for (int k = 1; k <= min(m, n) / 2; ++k) {
             for (int i = k; i < m - k; ++i) {
                 for (int j = k; j < n - k; ++j) {
-                    int top = i - k, bottom = i + k, left = j - k, right = j + k;
-                    
-                    int side1 = d2[i + 1][left + 1] - d2[top][j + 2]; 
-                    int side2 = d1[bottom + 1][j + 1] - d1[i][left];
-                    int side3 = d2[bottom + 1][j + 1] - d2[i][right + 2];
-                    int side4 = d1[i + 1][right + 1] - d1[top][j];
+                    int topR = i - k, topC = j;
+                    int botR = i + k, botC = j;
+                    int lR = i, lC = j - k;
+                    int rR = i, rC = j + k;
 
-                    int total = side1 + side2 + side3 + side4 - grid[top][j] - grid[bottom][j] - grid[i][left] - grid[i][right];
-                    
+                    // Sum 4 edges using prefix sums
+                    int s1 = upRight[lR + 1][lC + 1] - upRight[topR][topC + 2]; 
+                    int s2 = upLeft[botR + 1][botC + 1] - upLeft[lR][lC];      
+                    int s3 = upRight[botR + 1][botC + 1] - upRight[rR][rC + 2];
+                    int s4 = upLeft[rR + 1][rC + 1] - upLeft[topR][topC];     
+
+                    // Subtract the 4 corners because each was added twice
+                    int total = s1 + s2 + s3 + s4 - grid[topR][topC] - grid[botR][botC] - grid[lR][lC] - grid[rR][rC];
+
                     top3.insert(total);
                     if (top3.size() > 3) top3.erase(top3.begin());
                 }
