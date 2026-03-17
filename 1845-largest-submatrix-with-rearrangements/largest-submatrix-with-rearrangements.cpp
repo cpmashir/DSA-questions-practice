c78fd1b1f4a424788ac3de7cd99652b43da1bc12
cpmashir <ashircpm@gmail.com>
2026-03-17 23:00:25 +0530

    Time: 33 ms (63.83%) | Memory: 90.3 MB (34.57%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -4,13 +4,10 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int maxArea = 0;
-        
-        // Use a single vector to store current heights to save memory
         vector<int> heights(n, 0);
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                // Update heights: if 1, increment; if 0, reset
                 if (matrix[i][j] == 1) {
                     heights[j]++;
                 } else {
@@ -18,14 +15,11 @@ public:
                 }
             }
 
-            // Copy heights to a temporary vector for sorting
-            // Sorting is O(N log N), which is fine since N is the width
             vector<int> sortedHeights = heights;
             sort(sortedHeights.begin(), sortedHeights.end(), greater<int>());
 
-            // Calculate max area for this row
             for (int k = 0; k < n; k++) {
-                if (sortedHeights[k] == 0) break; // Optimization: no more 1s
+                if (sortedHeights[k] == 0) break;
                 maxArea = max(maxArea, sortedHeights[k] * (k + 1));
             }
         }
