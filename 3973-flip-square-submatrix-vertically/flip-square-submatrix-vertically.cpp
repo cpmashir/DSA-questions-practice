e1a89ef15ea921f7fa49c4f9b9b0c9d2b8813ca8
cpmashir <ashircpm@gmail.com>
2026-03-21 08:24:08 +0530

    Time: 0 ms (100.00%) | Memory: 34 MB (31.02%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.cpp
@@ -9,14 +9,10 @@ public:
         int top = x;
         int bottom = x + k - 1;
 
-        // Perform the vertical flip by swapping rows within the submatrix
         while (top < bottom) {
-            // Iterate through the columns within the submatrix's width k
             for (int j = y; j < y + k; ++j) {
                 swap(grid[top][j], grid[bottom][j]);
             }
-            
-            // Move the row pointers toward the center
             top++;
             bottom--;
         }
