37ab48605e06463161ef3a329fc172fb951dbb9c
cpmashir <ashircpm@gmail.com>
2026-03-24 18:26:55 +0530

    Time: 33 ms (71.59%) | Memory: 132.1 MB (71.96%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -7,31 +7,24 @@ public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int totalElements = n * m;
         int MOD = 12345;
         
         vector<vector<int>> p(n, vector<int>(m));
         int runningProduct = 1;
 
-        // Forward Pass: Calculate Prefix Products
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 p[i][j] = runningProduct;
-                // Pre-calculate grid value modulo 12345
-                int val = grid[i][j] % MOD;
-                runningProduct = (runningProduct * val) % MOD;
+                runningProduct = (runningProduct * (grid[i][j] % MOD)) % MOD;
             }
         }
 
         runningProduct = 1;
 
-        // Backward Pass: Multiply by Suffix Products
         for (int i = n - 1; i >= 0; --i) {
             for (int j = m - 1; j >= 0; --j) {
-                // p[i][j] currently holds the prefix product
                 p[i][j] = (p[i][j] * runningProduct) % MOD;
-                int val = grid[i][j] % MOD;
-                runningProduct = (runningProduct * val) % MOD;
+                runningProduct = (runningProduct * (grid[i][j] % MOD)) % MOD;
             }
         }
 
