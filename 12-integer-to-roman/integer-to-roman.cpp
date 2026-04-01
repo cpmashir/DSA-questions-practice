0da55148c9565e102b927b568d6bb3c3cafd361c
cpmashir <ashircpm@gmail.com>
2026-04-01 19:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (99.32%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        // Define the mapping of values and symbols in descending order
-        // We include the subtractive cases (4, 9, 40, etc.) as distinct entries
-        vector<pair<int, string>> romanMap = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        static const int values[] = {
+            1000, 900, 500, 400, 
+            100, 90, 50, 40, 
+            10, 9, 5, 4, 1
         };
-        
-        string result = "";
-        
-        for (int i = 0; i < romanMap.size(); i++) {
-            // While the number is larger than the current roman value
-            while (num >= romanMap[i].first) {
-                result += romanMap[i].second; // Add the symbol
-                num -= romanMap[i].first;    // Subtract the value
+        static const string symbols[] = {
+            "M", "CM", "D", "CD", 
+            "C", "XC", "L", "XL", 
+            "X", "IX", "V", "IV", "I"
+        };
+
+        string result;
+        result.reserve(15); 
+
+        for (int i = 0; i < 13; ++i) {
+            while (num >= values[i]) {
+                result += symbols[i];
+                num -= values[i];
             }
         }
-        
+
         return result;
     }
 };
