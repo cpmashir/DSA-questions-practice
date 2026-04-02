12623a8908bda6400b3c33b87b99d2487ca6f727
cpmashir <ashircpm@gmail.com>
2026-04-02 11:31:38 +0530

    Time: 96 ms (87.66%) | Memory: 106.4 MB (97.87%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -10,41 +10,41 @@ public:
         int n = coins[0].size();
         long long INF = 1e16;
         
-        vector<vector<long long>> prev(n, vector<long long>(3, -INF));
+        vector<vector<long long>> dp(n, vector<long long>(3, -INF));
 
         for (int i = 0; i < m; ++i) {
-            vector<vector<long long>> curr(n, vector<long long>(3, -INF));
             for (int j = 0; j < n; ++j) {
-                for (int k = 0; k < 3; ++k) {
-                    if (i == 0 && j == 0) {
-                        curr[0][0] = coins[0][0];
-                        curr[0][1] = max(0, coins[0][0]);
-                        curr[0][2] = max(0, coins[0][0]);
-                        break;
-                    }
+                if (i == 0 && j == 0) {
+                    dp[0][0] = coins[0][0];
+                    dp[0][1] = max(0, coins[0][0]);
+                    dp[0][2] = max(0, coins[0][0]);
+                    continue;
+                }
 
-                    long long best_prev = -INF;
-                    if (i > 0) best_prev = max(best_prev, prev[j][k]);
-                    if (j > 0) best_prev = max(best_prev, curr[j - 1][k]);
+                for (int k = 2; k >= 0; --k) {
+                    long long stay = dp[j][k]; 
+                    long long left = (j > 0) ? dp[j - 1][k] : -INF;
+                    long long best_prev = max(stay, left);
 
+                    long long res = -INF;
                     if (best_prev != -INF) {
-                        curr[j][k] = max(curr[j][k], best_prev + coins[i][j]);
+                        res = best_prev + coins[i][j];
                     }
 
                     if (k > 0) {
-                        long long best_prev_k = -INF;
-                        if (i > 0) best_prev_k = max(best_prev_k, prev[j][k - 1]);
-                        if (j > 0) best_prev_k = max(best_prev_k, curr[j - 1][k - 1]);
+                        long long stay_k = dp[j][k - 1];
+                        long long left_k = (j > 0) ? dp[j - 1][k - 1] : -INF;
+                        long long best_prev_k = max(stay_k, left_k);
                         
                         if (best_prev_k != -INF) {
-                            curr[j][k] = max(curr[j][k], best_prev_k + max(0, coins[i][j]));
+                            res = max(res, best_prev_k + max(0, coins[i][j]));
                         }
                     }
+                    dp[j][k] = res;
                 }
             }
-            prev = move(curr);
         }
 
-        return max({prev[n - 1][0], prev[n - 1][1], prev[n - 1][2]});
+        return (int)max({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]});
     }
 };
