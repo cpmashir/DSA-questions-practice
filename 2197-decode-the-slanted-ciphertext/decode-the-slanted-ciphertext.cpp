e84dafd498b9a1632c85f11780febc0ba6163327
cpmashir <ashircpm@gmail.com>
2026-04-04 12:30:02 +0530

    Time: 32 ms (56.78%) | Memory: 39.1 MB (77.12%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
         int n = encodedText.length();
-        if (n == 0) return "";
-        
         int cols = n / rows;
         string res;
         res.reserve(n);
 
         for (int i = 0; i < cols; ++i) {
-            int curr = i;
-            for (int r = 0; r < rows && curr < n; ++r) {
+            for (int curr = i; curr < n; curr += cols + 1) {
                 res += encodedText[curr];
-                curr += (cols + 1);
-                if (curr % cols == 0 && r < rows - 1) break; 
+                if ((curr - i) / (cols + 1) == rows - 1) break;
             }
         }
 
         int last = res.find_last_not_of(' ');
-        if (last == string::npos) return "";
-        res.erase(last + 1);
-
-        return res;
+        return (last == string::npos) ? "" : res.substr(0, last + 1);
     }
 };
