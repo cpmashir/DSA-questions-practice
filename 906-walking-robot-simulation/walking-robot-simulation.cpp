41f9499277cc5b78fcfb7dd523a189903e9a6d77
cpmashir <ashircpm@gmail.com>
2026-04-06 19:47:31 +0530

    Time: 19 ms (83.47%) | Memory: 36.8 MB (91.47%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,9 +1,3 @@
-#include <vector>
-#include <unordered_set>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
