81b7438490236cf2e3eb362288e7b008d9291761
cpmashir <ashircpm@gmail.com>
2026-04-08 10:00:31 +0530

    Time: 54 ms (85.65%) | Memory: 158 MB (91.87%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
@@ -1,33 +1,21 @@
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        long long MOD = 1e9 + 7;
-        int n = nums.size();
+        const int MOD = 1e9 + 7;
 
-        // Process each query
-        for (const auto& query : queries) {
-            int l = query[0];
-            int r = query[1];
-            int k = query[2];
-            long long v = query[3];
+        for (const auto& q : queries) {
+            int l = q[0], r = q[1], k = q[2];
+            long long v = q[3];
+            
+            if (v == 1) continue; 
 
-            // Update array elements based on the start index, range, and step
-            for (int idx = l; idx <= r; idx += k) {
-                // Perform multiplication and modulo operation
-                nums[idx] = (1LL * nums[idx] * v) % MOD;
+            for (int i = l; i <= r; i += k) {
+                nums[i] = (1LL * nums[i] * v) % MOD;
             }
         }
 
-        // Calculate the bitwise XOR of all final elements
-        int xorSum = 0;
-        for (int num : nums) {
-            xorSum ^= num;
-        }
-
-        return xorSum;
+        int res = 0;
+        for (int x : nums) res ^= x;
+        return res;
     }
 };
