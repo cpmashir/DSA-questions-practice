ec18e38526d3e2d6c4ab1a67342ca883fa2788c5
cpmashir <ashircpm@gmail.com>
2026-04-10 08:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (23.74%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
         int n = nums.size();
-        // Since nums[i] <= 100, we can use an array of vectors to store indices
         vector<int> positions[101];
         
         for (int i = 0; i < n; ++i) {
@@ -12,11 +11,9 @@ public:
         int min_dist = INT_MAX;
         bool found = false;
         
-        // Iterate through each value from 1 to 100
         for (int v = 1; v <= 100; ++v) {
             if (positions[v].size() >= 3) {
                 found = true;
-                // To minimize 2 * (k - i), we check consecutive triplets in the sorted indices
                 for (int p = 0; p <= (int)positions[v].size() - 3; ++p) {
                     int i = positions[v][p];
                     int k = positions[v][p + 2];
