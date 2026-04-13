e50abea9a714ab3849a747a48041cf4a28a58aaa
cpmashir <ashircpm@gmail.com>
2026-04-13 19:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (83.73%) - LeetSync

--- 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.cpp
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.cpp
@@ -5,15 +5,10 @@ public:
         
         for (int i = 0; i < nums.size(); ++i) {
             if (nums[i] == target) {
-                // Calculate the absolute distance
                 int current_dist = abs(i - start);
-                
-                // Update min_dist if the current one is smaller
                 if (current_dist < min_dist) {
                     min_dist = current_dist;
                 }
-                
-                // Optimization: If distance is 0, we can't do better
                 if (min_dist == 0) return 0;
             }
         }
