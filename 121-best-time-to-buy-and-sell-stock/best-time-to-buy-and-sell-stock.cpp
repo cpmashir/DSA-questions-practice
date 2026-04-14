15e5b34df29e95a9a68913251ff56d219ffec615
cpmashir <ashircpm@gmail.com>
2026-04-14 11:20:32 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (59.84%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,12 +5,9 @@ public:
         int maxProfit = 0;
         
         for (int i = 0; i < prices.size(); i++) {
-            // Update the minimum price seen so far
             if (prices[i] < minPrice) {
                 minPrice = prices[i];
-            } 
-            // Calculate profit if we sold today and update maxProfit
-            else if (prices[i] - minPrice > maxProfit) {
+            } else if (prices[i] - minPrice > maxProfit) {
                 maxProfit = prices[i] - minPrice;
             }
         }
