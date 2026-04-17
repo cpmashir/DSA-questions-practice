ec5132d47ff1a27a69a64c675f72d94254bc5aae
cpmashir <ashircpm@gmail.com>
2026-04-18 01:00:27 +0530

    Time: 107 ms (75.00%) | Memory: 131.8 MB (75.49%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,13 +1,5 @@
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-#include <climits>
-
-using namespace std;
-
 class Solution {
 public:
-    // Helper function to reverse digits of a number
     int reverseInt(int n) {
         long rev = 0;
         while (n > 0) {
@@ -18,21 +10,15 @@ public:
     }
 
     int minMirrorPairDistance(vector<int>& nums) {
-        // maps the value 'reverse(nums[i])' to the last seen index 'i'
         unordered_map<int, int> lastSeenReversed;
         int minDistance = INT_MAX;
         bool found = false;
 
         for (int j = 0; j < nums.size(); ++j) {
-            // Check if current nums[j] matches any reverse(nums[i]) seen before
             if (lastSeenReversed.count(nums[j])) {
                 minDistance = min(minDistance, j - lastSeenReversed[nums[j]]);
                 found = true;
             }
-
-            // Store/Update the reverse of the current number and its index
-            // We update it every time because a later index 'i' for the same 
-            // reversed value will always yield a smaller distance for future 'j's.
             lastSeenReversed[reverseInt(nums[j])] = j;
         }
 
