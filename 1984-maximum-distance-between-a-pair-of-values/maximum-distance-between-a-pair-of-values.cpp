250aa8a7d0303442c075bb09c156b9b53d44817a
cpmashir <ashircpm@gmail.com>
2026-04-19 08:38:52 +0530

    Time: 0 ms (100.00%) | Memory: 102.2 MB (47.28%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
@@ -7,15 +7,11 @@ public:
         int n2 = nums2.size();
 
         while (i < n1 && j < n2) {
-            // Check if the current pair is valid
             if (nums1[i] <= nums2[j]) {
-                // If valid, calculate distance and try to increase j
                 max_dist = max(max_dist, j - i);
                 j++;
             } else {
-                // If invalid, nums1[i] is too big, move i forward
                 i++;
-                // Optimization: Ensure j is at least at i to keep distance non-negative
                 if (j < i) {
                     j = i;
                 }
