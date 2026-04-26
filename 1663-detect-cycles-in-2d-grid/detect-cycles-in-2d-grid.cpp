0a42596e500d09224771d326848e746bdd78b4c7
cpmashir <ashircpm@gmail.com>
2026-04-26 17:18:18 +0530

    Time: 3 ms (100.00%) | Memory: 50 MB (98.74%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,51 +1,27 @@
 class Solution {
 public:
     bool containsCycle(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        int m = grid.size(), n = grid[0].size();
+        vector<int> parent(m * n);
+        iota(parent.begin(), parent.end(), 0);
+
+        auto find = [&](auto& self, int i) -> int {
+            return parent[i] == i ? i : parent[i] = self(self, parent[i]);
+        };
 
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                // If not visited, start a DFS to look for a cycle
-                if (!visited[i][j]) {
-                    if (dfs(grid, visited, i, j, -1, -1, grid[i][j])) {
-                        return true;
-                    }
+                if (i > 0 && grid[i][j] == grid[i - 1][j]) {
+                    int rootA = find(find, i * n + j);
+                    int rootB = find(find, (i - 1) * n + j);
+                    if (rootA == rootB) return true;
+                    parent[rootA] = rootB;
                 }
-            }
-        }
-        return false;
-    }
-
-private:
-    bool dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, 
-             int r, int c, int pr, int pc, char target) {
-        
-        visited[r][c] = true;
-        
-        // Directions: Up, Down, Left, Right
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
-
-        for (int i = 0; i < 4; ++i) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-
-            // 1. Check bounds and character match
-            if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() 
-                && grid[nr][nc] == target) {
-                
-                // 2. If neighbor is visited and NOT the parent, it's a cycle!
-                if (visited[nr][nc]) {
-                    if (nr != pr || nc != pc) {
-                        return true;
-                    }
-                } else {
-                    // 3. Continue DFS
-                    if (dfs(grid, visited, nr, nc, r, c, target)) {
-                        return true;
-                    }
+                if (j > 0 && grid[i][j] == grid[i][j - 1]) {
+                    int rootA = find(find, i * n + j);
+                    int rootB = find(find, i * n + (j - 1));
+                    if (rootA == rootB) return true;
+                    parent[rootA] = rootB;
                 }
             }
         }
