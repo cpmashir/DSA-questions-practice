05772e87a81c82c6a520e2f738c0058cbba983ad
cpmashir <ashircpm@gmail.com>
2026-04-27 13:01:52 +0530

    Time: 3 ms (98.79%) | Memory: 52.5 MB (89.11%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -1,33 +1,38 @@
 class Solution {
+    int masks[7] = {0, 10, 5, 12, 6, 9, 3};
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    bool visited[300][300];
+    pair<int, int> q[90005];
+
 public:
     bool hasValidPath(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        int masks[7] = {0, 10, 5, 12, 6, 9, 3};
-        int dx[4] = {-1, 0, 1, 0};
-        int dy[4] = {0, 1, 0, -1};
+        if (m == 1 && n == 1) return true;
 
-        queue<pair<int, int>> q;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for (int i = 0; i < m; ++i) 
+            for (int j = 0; j < n; ++j) 
+                visited[i][j] = false;
 
-        q.push({0, 0});
+        int head = 0, tail = 0;
+        q[tail++] = {0, 0};
         visited[0][0] = true;
 
-        while (!q.empty()) {
-            pair<int, int> curr = q.front();
+        while (head < tail) {
+            pair<int, int> curr = q[head++];
             int r = curr.first, c = curr.second;
-            q.pop();
 
             if (r == m - 1 && c == n - 1) return true;
 
+            int currentMask = masks[grid[r][c]];
             for (int i = 0; i < 4; ++i) {
-                if (masks[grid[r][c]] & (1 << i)) {
+                if (currentMask & (1 << i)) {
                     int nr = r + dx[i], nc = c + dy[i];
-                    
+
                     if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
-                        int backDir = (i + 2) % 4;
-                        if (masks[grid[nr][nc]] & (1 << backDir)) {
+                        if (masks[grid[nr][nc]] & (1 << ((i + 2) % 4))) {
                             visited[nr][nc] = true;
-                            q.push({nr, nc});
+                            q[tail++] = {nr, nc};
                         }
                     }
                 }
