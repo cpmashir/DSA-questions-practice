4da4c92d5565725057b47a66576da854d0a02060
cpmashir <ashircpm@gmail.com>
2026-05-07 13:42:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.62%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -6,36 +7,21 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> result;
-        vector<int> currentPath;
-        vector<bool> used(nums.size(), false);
-        
-        backtrack(nums, used, currentPath, result);
-        
+        backtrack(0, nums, result);
         return result;
     }
 
 private:
-    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& currentPath, vector<vector<int>>& result) {
-        // Base Case: If current path length matches nums length, we found a permutation
-        if (currentPath.size() == nums.size()) {
-            result.push_back(currentPath);
+    void backtrack(int start, vector<int>& nums, vector<vector<int>>& result) {
+        if (start == nums.size()) {
+            result.push_back(nums);
             return;
         }
 
-        for (int i = 0; i < nums.size(); ++i) {
-            // Skip if the number is already in the current permutation
-            if (used[i]) continue;
-
-            // 1. Action: Choose the number
-            used[i] = true;
-            currentPath.push_back(nums[i]);
-
-            // 2. Recurse: Move to the next slot
-            backtrack(nums, used, currentPath, result);
-
-            // 3. Backtrack: Undo the choice to try other numbers for this slot
-            currentPath.pop_back();
-            used[i] = false;
+        for (int i = start; i < nums.size(); ++i) {
+            swap(nums[start], nums[i]);
+            backtrack(start + 1, nums, result);
+            swap(nums[start], nums[i]);
         }
     }
 };
