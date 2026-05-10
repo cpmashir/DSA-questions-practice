e0499a1149a23034e919259e300a45e491ee71ba
cpmashir <ashircpm@gmail.com>
2026-05-10 16:58:30 +0530

    Time: 11 ms (93.79%) | Memory: 68.6 MB (40.06%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int> dp(n, -1);
+        // Using short saves 50% memory compared to int for the DP table
+        // -1 indicates unreachable
+        short dp[1000]; 
+        fill(dp, dp + n, -1);
         dp[0] = 0;
 
         for (int j = 1; j < n; ++j) {
-            int current_val = nums[j];
+            const int curr = nums[j];
             for (int i = 0; i < j; ++i) {
                 if (dp[i] != -1) {
-                    long long diff = (long long)current_val - nums[i];
+                    long long diff = (long long)curr - nums[i];
                     if (diff >= -target && diff <= target) {
                         if (dp[i] + 1 > dp[j]) {
                             dp[j] = dp[i] + 1;
@@ -18,6 +21,6 @@ public:
                 }
             }
         }
-        return dp[n - 1];
+        return (int)dp[n - 1];
     }
 };
