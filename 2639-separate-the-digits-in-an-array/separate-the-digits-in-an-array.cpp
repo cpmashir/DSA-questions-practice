2b0c13eea9f4c3c70db675655354e4c4b55dc38a
cpmashir <ashircpm@gmail.com>
2026-05-11 13:03:31 +0530

    Time: 13 ms (5.63%) | Memory: 16 MB (37.58%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<int> separateDigits(vector<int>& nums) {
         vector<int> answer;
-        
         for (int val : nums) {
-            // Convert the number to a string to easily access digits in order
-            string s = to_string(val);
-            for (char c : s) {
-                // Convert char back to int ('0' is ASCII 48)
-                answer.push_back(c - '0');
+            vector<int> temp;
+            while (val > 0) {
+                temp.push_back(val % 10);
+                val /= 10;
+            }
+            // Reverse the digits and add to the main answer
+            for (int i = temp.size() - 1; i >= 0; i--) {
+                answer.push_back(temp[i]);
             }
         }
-        
         return answer;
     }
 };
