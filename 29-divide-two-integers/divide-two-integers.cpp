2ac6ec0b4b26d94642bfce7fe1c6b6c36dcdc4d5
cpmashir <ashircpm@gmail.com>
2026-05-12 05:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (37.06%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -2,21 +2,28 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        if (divisor == 1) return dividend;
 
-        bool neg = (dividend > 0) ^ (divisor > 0);
-        
-        // Cast to unsigned int BEFORE negation to handle INT_MIN safely
-        unsigned int a = (dividend == INT_MIN) ? (unsigned int)INT_MAX + 1 : abs(dividend);
-        unsigned int b = (divisor == INT_MIN) ? (unsigned int)INT_MAX + 1 : abs(divisor);
-        unsigned int res = 0;
+        bool negative = (dividend > 0) ^ (divisor > 0);
 
-        for (int i = 31; i >= 0; i--) {
-            if ((a >> i) >= b) {
-                res += (1U << i);
-                a -= (b << i);
+        if (dividend > 0) dividend = -dividend;
+        if (divisor > 0) divisor = -divisor;
+
+        int quotient = 0;
+
+        while (dividend <= divisor) {
+            int tempDivisor = divisor;
+            int multiple = 1;
+
+            while (tempDivisor >= (INT_MIN >> 1) && dividend <= (tempDivisor << 1)) {
+                tempDivisor <<= 1;
+                multiple <<= 1;
             }
+
+            dividend -= tempDivisor;
+            quotient += multiple;
         }
 
-        return neg ? -res : res;
+        return negative ? -quotient : quotient;
     }
 };
