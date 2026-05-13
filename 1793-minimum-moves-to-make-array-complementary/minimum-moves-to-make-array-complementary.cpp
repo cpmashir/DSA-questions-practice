cc1830fc25ea947105d143c8a9def7e93c6ccdb3
cpmashir <ashircpm@gmail.com>
2026-05-13 12:39:21 +0530

    Time: 6 ms (73.45%) | Memory: 85.8 MB (97.35%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -1,58 +1,38 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 static const int fast_io = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
     return 0;
 }();
+
+int diff[200002];
 
 class Solution {
 public:
     int minMoves(vector<int>& nums, int limit) {
         int n = nums.size();
-        // We only need 2 * limit + 2 slots. 
-        // Using a vector is fine, but we can pre-allocate.
-        vector<int> diff(2 * limit + 2, 0);
+        int max_sum = 2 * limit;
+        
+        for (int i = 0; i <= max_sum + 1; ++i) diff[i] = 0;
 
         for (int i = 0; i < n / 2; ++i) {
             int a = nums[i];
             int b = nums[n - 1 - i];
+            
+            int mn = a < b ? a : b;
+            int mx = a > b ? a : b;
 
-            // For each pair, we determine the ranges of target sum S:
-            // 2 moves: [2, min(a, b)]
-            // 1 move:  [min(a, b) + 1, a + b - 1]
-            // 0 moves: a + b
-            // 1 move:  [a + b + 1, max(a, b) + limit]
-            // 2 moves: [max(a, b) + limit + 1, 2 * limit]
-
-            // Optimized update:
-            // Start by assuming 2 moves for everything:
             diff[2] += 2;
-            
-            // Discount 1 move for the [min+1, max+limit] range:
-            int low = min(a, b) + 1;
-            int high = max(a, b) + limit;
-            diff[low] -= 1;
-            diff[high + 1] += 1;
-            
-            // Discount another 1 move for the exact sum:
-            int target = a + b;
-            diff[target] -= 1;
-            diff[target + 1] += 1;
+            diff[mn + 1] -= 1;
+            diff[a + b] -= 1;
+            diff[a + b + 1] += 1;
+            diff[mx + limit + 1] += 1;
         }
 
-        int min_moves = n; 
-        int current_moves = 0;
-        
-        // Linear sweep to find the minimum point in the prefix sum array
-        for (int i = 2; i <= 2 * limit; ++i) {
-            current_moves += diff[i];
-            if (current_moves < min_moves) {
-                min_moves = current_moves;
-            }
+        int min_moves = n;
+        int curr = 0;
+        for (int i = 2; i <= max_sum; ++i) {
+            curr += diff[i];
+            if (curr < min_moves) min_moves = curr;
         }
 
         return min_moves;
