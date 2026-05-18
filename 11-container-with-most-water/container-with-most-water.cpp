1cbb38c94695db1b2c219990fe0bdd72bdd39a24
cpmashir <ashircpm@gmail.com>
2026-05-18 14:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (10.12%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,42 +1,24 @@
-#pragma GCC optimize("O3")
-
-auto speedup = []() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    return 0;
-}();
-
 class Solution {
 public:
-    int maxArea(std::vector<int>& height) {
-        int max_water = 0;
-        
-        // Use raw pointers for ultra-fast calculation
-        const int* left = height.data();
-        const int* right = left + height.size() - 1;
-        
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxWater = 0;
+
         while (left < right) {
-            int h_left = *left;
-            int h_right = *right;
-            
-            int current_water = (right - left) * (h_left < h_right ? h_left : h_right);
-            if (current_water > max_water) {
-                max_water = current_water;
-            }
-            
-            if (h_left < h_right) {
-                while (left < right && *left <= h_left) left++;
+            int width = right - left;
+            int h = min(height[left], height[right]);
+
+            maxWater = max(maxWater, width * h);
+
+            // Move the smaller height pointer
+            if (height[left] < height[right]) {
+                left++;
             } else {
-                while (left < right && *right <= h_right) right--;
+                right--;
             }
         }
-        
-        // THE MEMORY HACK:
-        // Clear the vector and force deallocation of its capacity 
-        // back to the operating system before returning.
-        height.clear();
-        std::vector<int>().swap(height); 
-        
-        return max_water;
+
+        return maxWater;
     }
 };
