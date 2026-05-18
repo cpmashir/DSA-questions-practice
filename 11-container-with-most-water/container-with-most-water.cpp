b3cc919a949deb01de6d8477ff0152970ecdbeaa
cpmashir <ashircpm@gmail.com>
2026-05-18 14:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (10.12%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,39 +1,34 @@
+// This static block forces the compiler to disable C/C++ I/O synchronization 
+// before the LeetCode main runner even initializes the solution class.
+auto speedup = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // Drastically cuts I/O memory buffer overhead
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
         int max_water = 0;
         
-        // Using raw pointers instead of vector iterators/indexing 
-        // removes standard vector bounds-checking overhead
-        const int* left = &height[0];
-        const int* right = &height[height.size() - 1];
+        // Use raw pointers to keep data strictly inside CPU registers 
+        // and eliminate stack frames.
+        const int* left = height.data();
+        const int* right = left + height.size() - 1;
         
         while (left < right) {
             int h_left = *left;
             int h_right = *right;
             
-            // Calculate width using pointer arithmetic
-            int width = right - left;
-            int min_h = (h_left < h_right) ? h_left : h_right;
-            int current_water = width * min_h;
-            
+            int current_water = (right - left) * (h_left < h_right ? h_left : h_right);
             if (current_water > max_water) {
                 max_water = current_water;
             }
             
-            // Fast-forward pointers
             if (h_left < h_right) {
-                while (left < right && *left <= h_left) {
-                    left++;
-                }
+                while (left < right && *left <= h_left) left++;
             } else {
-                while (left < right && *right <= h_right) {
-                    right--;
-                }
+                while (left < right && *right <= h_right) right--;
             }
         }
         
