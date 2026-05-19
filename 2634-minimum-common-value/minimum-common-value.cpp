7e3dd54e7395d9000dd37a652cc3d37d7790b7fb
cpmashir <ashircpm@gmail.com>
2026-05-19 12:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (99.84%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,10 +1,18 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int i = 0;
         int j = 0;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
         
-        while (i < nums1.size() && j < nums2.size()) {
+        while (i < n1 && j < n2) {
             if (nums1[i] == nums2[j]) {
                 return nums1[i];
             }
