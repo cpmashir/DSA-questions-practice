52ad273c1290c6828fe5e606d3158f72aa53d6ff
cpmashir <ashircpm@gmail.com>
2026-05-24 14:36:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.98%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,28 +4,25 @@ public:
         int n = nums.size();
         int left = -1, right = -1;
         
-        // 1. Find Left Bound
         int low = 0, high = n - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] >= target) {
                 if (nums[mid] == target) left = mid;
-                high = mid - 1; // Force search to the left side
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
         
-        // If target wasn't found in the first pass, it's not in the array
         if (left == -1) return {-1, -1};
         
-        // 2. Find Right Bound (Reuse variables to save stack memory)
         low = left, high = n - 1; 
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] <= target) {
                 if (nums[mid] == target) right = mid;
-                low = mid + 1; // Force search to the right side
+                low = mid + 1;
             } else {
                 high = mid - 1;
             }
