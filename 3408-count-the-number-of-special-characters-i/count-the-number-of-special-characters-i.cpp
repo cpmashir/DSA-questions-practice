29e5e0a171b8ddefe1fdd19c2b2ec0cb45afeb80
cpmashir <ashircpm@gmail.com>
2026-05-26 09:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (96.06%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<bool> lower(26, false);
+        vector<bool> upper(26, false);
+        
+        // Mark the presence of each character
+        for (char c : word) {
+            if (c >= 'a' && c <= 'z') {
+                lower[c - 'a'] = true;
+            } else if (c >= 'A' && c <= 'Z') {
+                upper[c - 'A'] = true;
+            }
+        }
+        
+        // Count how many pairs exist
+        int specialCount = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (lower[i] && upper[i]) {
+                specialCount++;
+            }
+        }
+        
+        return specialCount;
+    }
+};
