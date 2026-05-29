efad086d4abfb1ce3f7cd2821fa7989399a43a8a
cpmashir <ashircpm@gmail.com>
2026-05-29 09:40:32 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (96.60%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.cpp
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.cpp
@@ -6,13 +6,11 @@ public:
         for (int num : nums) {
             int digit_sum = 0;
             
-            // Calculate the sum of digits for the current number
             while (num > 0) {
                 digit_sum += num % 10;
                 num /= 10;
             }
             
-            // Track the minimum digit sum encountered so far
             min_val = min(min_val, digit_sum);
         }
         
