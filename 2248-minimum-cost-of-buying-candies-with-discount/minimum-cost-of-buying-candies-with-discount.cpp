60ce47cf33e1a26387771999eb426debe707d6c4
cpmashir <ashircpm@gmail.com>
2026-06-01 10:31:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (49.81%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int minimumCost(std::vector<int>& cost) {
-        // Sort the costs in descending order
         std::sort(cost.begin(), cost.end(), std::greater<int>());
         
         int totalCost = 0;
         
-        // Loop through the sorted candies
         for (int i = 0; i < cost.size(); ++i) {
-            // Every 3rd candy (index 2, 5, 8...) is free, so we skip it
             if ((i + 1) % 3 == 0) {
                 continue;
             }
