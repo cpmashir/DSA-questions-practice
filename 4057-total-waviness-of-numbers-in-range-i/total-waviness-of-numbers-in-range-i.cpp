f32f9f5e4538e3f3a9253a5dee4928f50383bd8f
cpmashir <ashircpm@gmail.com>
2026-06-04 17:49:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (96.46%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -4,31 +4,24 @@ public:
         int total_sum = 0;
         
         for (int i = num1; i <= num2; ++i) {
-            // Any number with fewer than 3 digits (0 to 99) has a waviness of 0
             if (i < 100) continue;
             
             int temp = i;
-            
-            // Extract the first two digits from the right
             int right = temp % 10;
             temp /= 10;
             int curr = temp % 10;
             temp /= 10;
             
-            // Iterate through the remaining digits
             while (temp > 0) {
                 int left = temp % 10;
                 
-                // Check if 'curr' is a Peak
                 if (curr > left && curr > right) {
                     total_sum++;
                 }
-                // Check if 'curr' is a Valley
                 else if (curr < left && curr < right) {
                     total_sum++;
                 }
                 
-                // Shift windows to the left for the next iteration
                 right = curr;
                 curr = left;
                 temp /= 10;
