904d93c10bd6eea6afaa4e477106a87f99fd6dc3
cpmashir <ashircpm@gmail.com>
2026-06-07 02:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (81.43%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -1,14 +1,9 @@
-#include <vector>
-#include <numeric>
-#include <cmath>
-
 class Solution {
 public:
     std::vector<int> leftRightDifference(std::vector<int>& nums) {
         int n = nums.size();
         std::vector<int> answer(n);
         
-        // Calculate the total sum of the array
         int totalSum = 0;
         for (int num : nums) {
             totalSum += num;
@@ -18,13 +13,8 @@ public:
         int rightSum = totalSum;
         
         for (int i = 0; i < n; ++i) {
-            // rightSum for index i is the total remaining sum minus the current element
             rightSum -= nums[i];
-            
-            // Calculate the absolute difference
             answer[i] = std::abs(leftSum - rightSum);
-            
-            // Update leftSum for the next index
             leftSum += nums[i];
         }
         
