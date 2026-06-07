b52b494c6e0c015eee549330b87a43a4d7d38379
cpmashir <ashircpm@gmail.com>
2026-06-07 08:58:02 +0530

    Time: 30 ms (95.87%) | Memory: 234.5 MB (95.01%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        // Fast I/O operations
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        // Max node value is 100,000 based on constraints
         const int MAX_VAL = 100001;
-        
-        // Direct-address table for tree node pointers
-        // Using a flat array eliminates hash-map overhead
         TreeNode* nodeMap[MAX_VAL] = {nullptr};
-        
-        // Array to track if a node value has a parent
         bool hasParent[MAX_VAL] = {false};
 
         for (const auto& desc : descriptions) {
@@ -20,27 +13,22 @@ public:
             int cVal = desc[1];
             int isLeft = desc[2];
 
-            // Allocate parent if it doesn't exist yet
             if (!nodeMap[pVal]) {
                 nodeMap[pVal] = new TreeNode(pVal);
             }
-            // Allocate child if it doesn't exist yet
             if (!nodeMap[cVal]) {
                 nodeMap[cVal] = new TreeNode(cVal);
             }
 
-            // Link them directly
             if (isLeft) {
                 nodeMap[pVal]->left = nodeMap[cVal];
             } else {
                 nodeMap[pVal]->right = nodeMap[cVal];
             }
 
-            // Mark this child as having a parent
             hasParent[cVal] = true;
         }
 
-        // Find the root: look up the parent values present in descriptions
         for (const auto& desc : descriptions) {
             int pVal = desc[0];
             if (!hasParent[pVal]) {
