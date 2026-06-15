442dbdbf940b75e8071c646875ee068e4ff86bde
cpmashir <ashircpm@gmail.com>
2026-06-15 08:14:34 +0530

    Time: 0 ms (100.00%) | Memory: 312.1 MB (54.78%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if (!head->next) return nullptr;
+        if (!head || !head->next)
+            return nullptr;
 
-        ListNode *slow = head, *fast = head->next->next;
+        ListNode* slow = head;
+        ListNode* fast = head->next->next;
 
         while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        ListNode* temp = slow->next;
         slow->next = slow->next->next;
-        delete temp;
-
         return head;
     }
 };
