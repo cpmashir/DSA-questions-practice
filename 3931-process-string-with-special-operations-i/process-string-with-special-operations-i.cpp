dd4d6f07387dd40bdf0888d9a2a205ade4630432
cpmashir <ashircpm@gmail.com>
2026-06-17 00:02:17 +0530

    Time: 6 ms (39.02%) | Memory: 39.3 MB (98.48%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -4,23 +4,32 @@
 class Solution {
 public:
     string processStr(string s) {
-        string result = "";
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         
-        for (char c : s) {
-            if (c == '*') {
-                // Remove the last character if the result is not empty
-                if (!result.empty()) {
-                    result.pop_back();
-                }
-            } else if (c == '#') {
-                // Duplicate the current result and append it to itself
-                result += result;
-            } else if (c == '%') {
-                // Reverse the current result
-                std::reverse(result.begin(), result.end());
-            } else {
-                // It's a lowercase English letter, so append it
-                result.push_back(c);
+        string result = "";
+        result.reserve(128); 
+        
+        for (const char c : s) {
+            switch (c) {
+                case '*':
+                    if (!result.empty()) {
+                        result.pop_back();
+                    }
+                    break;
+                case '#':
+                    if (!result.empty()) {
+                        result.append(result);
+                    }
+                    break;
+                case '%':
+                    if (result.length() > 1) {
+                        std::reverse(result.begin(), result.end());
+                    }
+                    break;
+                default:
+                    result.push_back(c);
+                    break;
             }
         }
         
