1ac77a78633fc5273aad2d3b7f31d94515ffa591
cpmashir <ashircpm@gmail.com>
2026-06-19 08:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (21.00%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -4,7 +4,6 @@ public:
         int max_alt = 0;
         int current_alt = 0;
         
-        // Using a reference to avoid any iterator or indexing overhead
         for (const int& g : gain) {
             current_alt += g;
             if (current_alt > max_alt) {
