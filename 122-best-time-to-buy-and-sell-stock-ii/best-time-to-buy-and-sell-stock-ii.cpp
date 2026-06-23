f720fc33f25969cf8ccf13e56cffa3474cce7ab7
cpmashir <ashircpm@gmail.com>
2026-06-23 09:14:52 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (63.12%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,7 +4,6 @@ public:
         int max_profit = 0;
         
         for (size_t i = 1; i < prices.size(); ++i) {
-            // If the price today is higher than yesterday, take the profit
             if (prices[i] > prices[i - 1]) {
                 max_profit += prices[i] - prices[i - 1];
             }
