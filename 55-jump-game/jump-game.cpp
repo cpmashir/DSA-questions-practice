a9a749e279c4cec79c4a2f59a7faee2adefea2aa
cpmashir <ashircpm@gmail.com>
2026-06-24 10:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (58.13%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -5,16 +5,12 @@ public:
         int n = nums.size();
         
         for (int i = 0; i < n; ++i) {
-            // If the current index is beyond the maximum reachable index,
-            // it means we can't even get to this point.
             if (i > maxReachable) {
                 return false;
             }
             
-            // Update the furthest index we can reach from the current position
             maxReachable = max(maxReachable, i + nums[i]);
             
-            // Optimization: If we can already reach or exceed the last index, return true
             if (maxReachable >= n - 1) {
                 return true;
             }
