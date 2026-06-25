6937f6b6abf8bdbae392f03f05565849bea8d479
cpmashir <ashircpm@gmail.com>
2026-06-25 21:23:37 +0530

    Time: 7 ms (93.90%) | Memory: 40.7 MB (40.52%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -1,41 +1,30 @@
 class Solution {
-private:
-    vector<int> bit;
-    int size;
-
-    void update(int idx, int val) {
-        for (; idx <= size; idx += idx & -idx) {
-            bit[idx] += val;
-        }
-    }
-
-    int query(int idx) {
-        int sum = 0;
-        for (; idx > 0; idx -= idx & -idx) {
-            sum += bit[idx];
-        }
-        return sum;
-    }
-
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         
         int n = nums.size();
-        size = 2 * n + 5;
-        bit.assign(size, 0);
+        int freq[2005]; 
+        memset(freq, 0, sizeof(freq));
 
         int count = 0;
         int prefix_sum = 0;
-        int offset = n + 2;
+        int offset = n;
+        
+        freq[offset] = 1;
+        int valid_subarrays_count = 0;
 
-        update(offset, 1);
-
-        for (int num : nums) {
-            prefix_sum += (num == target) ? 1 : -1;
-            count += query(prefix_sum + offset - 1);
-            update(prefix_sum + offset, 1);
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                valid_subarrays_count += freq[prefix_sum + offset];
+                prefix_sum++;
+            } else {
+                prefix_sum--;
+                valid_subarrays_count -= freq[prefix_sum + offset];
+            }
+            count += valid_subarrays_count;
+            freq[prefix_sum + offset]++;
         }
 
         return count;
