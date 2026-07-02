935155acef0703534805a4fc6c105bbb2c275c1a
cpmashir <ashircpm@gmail.com>
2026-07-02 17:28:41 +0530

    Time: 7 ms (92.60%) | Memory: 32 MB (57.32%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -2,36 +2,38 @@ class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size(), n = grid[0].size();
-        const int INF = 1e9;
-
-        vector<vector<int>> dist(m, vector<int>(n, INF));
+        vector<vector<char>> dist(m, vector<char>(n, 127));
         deque<pair<int, int>> dq;
 
         dist[0][0] = grid[0][0];
-        dq.push_front({0, 0});
+        dq.emplace_front(0, 0);
 
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
+        static const int d[5] = {-1, 0, 1, 0, -1};
 
         while (!dq.empty()) {
-            auto [r, c] = dq.front();
+            auto [x, y] = dq.front();
             dq.pop_front();
 
-            for (int k = 0; k < 4; k++) {
-                int nr = r + dr[k], nc = c + dc[k];
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+            char cur = dist[x][y];
+            if (x == m - 1 && y == n - 1)
+                return cur < health;
 
-                int w = grid[nr][nc];
-                if (dist[r][c] + w < dist[nr][nc]) {
-                    dist[nr][nc] = dist[r][c] + w;
-                    if (w == 0)
-                        dq.push_front({nr, nc});
-                    else
-                        dq.push_back({nr, nc});
-                }
+            for (int k = 0; k < 4; ++k) {
+                int nx = x + d[k], ny = y + d[k + 1];
+                if ((unsigned)nx >= (unsigned)m || (unsigned)ny >= (unsigned)n)
+                    continue;
+
+                char nd = cur + grid[nx][ny];
+                if (nd >= dist[nx][ny]) continue;
+
+                dist[nx][ny] = nd;
+                if (grid[nx][ny])
+                    dq.emplace_back(nx, ny);
+                else
+                    dq.emplace_front(nx, ny);
             }
         }
 
-        return dist[m - 1][n - 1] < health;
+        return false;
     }
 };
