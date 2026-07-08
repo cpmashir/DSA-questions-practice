38cd081291e360baa6587f4b5b1a6d2b6b1a942b
cpmashir <ashircpm@gmail.com>
2026-07-08 18:44:54 +0530

    Time: 8 ms (99.48%) | Memory: 143.2 MB (90.16%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -3,44 +3,44 @@ public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
         const int MOD = 1000000007;
 
-        vector<int> idx, d;
-        idx.reserve(s.size());
-        d.reserve(s.size());
+        int m = s.size();
+        vector<int> rank(m + 1);
+        vector<int> digit;
+        digit.reserve(m);
 
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] != '0') {
-                idx.push_back(i);
-                d.push_back(s[i] - '0');
-            }
+        for (int i = 0; i < m; ++i) {
+            rank[i] = digit.size();
+            if (s[i] != '0')
+                digit.push_back(s[i] - '0');
         }
+        rank[m] = digit.size();
 
-        int n = d.size();
-        vector<int> pw(n + 1), preVal(n + 1), preSum(n + 1);
-        pw[0] = 1;
+        int n = digit.size();
+
+        vector<int> pw(n + 1, 1), val(n + 1), sum(n + 1);
 
         for (int i = 0; i < n; ++i) {
             pw[i + 1] = (long long)pw[i] * 10 % MOD;
-            preVal[i + 1] = ((long long)preVal[i] * 10 + d[i]) % MOD;
-            preSum[i + 1] = preSum[i] + d[i];
+            val[i + 1] = ((long long)val[i] * 10 + digit[i]) % MOD;
+            sum[i + 1] = sum[i] + digit[i];
         }
 
         vector<int> ans;
         ans.reserve(queries.size());
 
         for (auto &q : queries) {
-            int l = lower_bound(idx.begin(), idx.end(), q[0]) - idx.begin();
-            int r = upper_bound(idx.begin(), idx.end(), q[1]) - idx.begin();
+            int L = rank[q[0]];
+            int R = rank[q[1] + 1];
 
-            if (l == r) {
+            if (L == R) {
                 ans.push_back(0);
                 continue;
             }
 
-            int len = r - l;
-            long long x = preVal[r] - (long long)preVal[l] * pw[len] % MOD;
+            long long x = val[R] - (long long)val[L] * pw[R - L] % MOD;
             if (x < 0) x += MOD;
 
-            ans.push_back(x * (preSum[r] - preSum[l]) % MOD);
+            ans.push_back(x * (sum[R] - sum[L]) % MOD);
         }
 
         return ans;
