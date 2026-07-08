e9fbd45baa792e32124620d73fa11aa98fbbf5aa
cpmashir <ashircpm@gmail.com>
2026-07-08 18:43:54 +0530

    Time: 71 ms (32.64%) | Memory: 142.5 MB (90.16%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-        static const int MOD = 1000000007;
+        const int MOD = 1000000007;
 
-        vector<int> pos, digit;
-        for (int i = 0; i < (int)s.size(); i++) {
+        vector<int> idx, d;
+        idx.reserve(s.size());
+        d.reserve(s.size());
+
+        for (int i = 0; i < s.size(); ++i) {
             if (s[i] != '0') {
-                pos.push_back(i);
-                digit.push_back(s[i] - '0');
+                idx.push_back(i);
+                d.push_back(s[i] - '0');
             }
         }
 
-        int n = digit.size();
-        vector<long long> pow10(n + 1, 1), prefVal(n + 1, 0), prefSum(n + 1, 0);
+        int n = d.size();
+        vector<int> pw(n + 1), preVal(n + 1), preSum(n + 1);
+        pw[0] = 1;
 
-        for (int i = 0; i < n; i++) {
-            pow10[i + 1] = (pow10[i] * 10) % MOD;
-            prefVal[i + 1] = (prefVal[i] * 10 + digit[i]) % MOD;
-            prefSum[i + 1] = prefSum[i] + digit[i];
+        for (int i = 0; i < n; ++i) {
+            pw[i + 1] = (long long)pw[i] * 10 % MOD;
+            preVal[i + 1] = ((long long)preVal[i] * 10 + d[i]) % MOD;
+            preSum[i + 1] = preSum[i] + d[i];
         }
 
         vector<int> ans;
         ans.reserve(queries.size());
 
         for (auto &q : queries) {
-            int l = lower_bound(pos.begin(), pos.end(), q[0]) - pos.begin();
-            int r = upper_bound(pos.begin(), pos.end(), q[1]) - pos.begin();
+            int l = lower_bound(idx.begin(), idx.end(), q[0]) - idx.begin();
+            int r = upper_bound(idx.begin(), idx.end(), q[1]) - idx.begin();
 
             if (l == r) {
                 ans.push_back(0);
@@ -33,11 +37,10 @@ public:
             }
 
             int len = r - l;
-            long long x = (prefVal[r] - prefVal[l] * pow10[len]) % MOD;
+            long long x = preVal[r] - (long long)preVal[l] * pw[len] % MOD;
             if (x < 0) x += MOD;
 
-            long long sum = prefSum[r] - prefSum[l];
-            ans.push_back((x * sum) % MOD);
+            ans.push_back(x * (preSum[r] - preSum[l]) % MOD);
         }
 
         return ans;
