1660f101bc9b01f2c1a1c0425ff379f33ef2477b
C P M Ashir <ashircpm@gmail.com>
2026-07-22 10:51:18 +0530

    Time: 7 ms (60.83%) | Memory: 10.4 MB (96.96%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+
+        vector<int> prev(n + 1), cur(n + 1);
+
+        for (int j = 0; j <= n; j++)
+            prev[j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            cur[0] = i;
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    cur[j] = prev[j - 1];
+                else
+                    cur[j] = 1 + min({prev[j], cur[j - 1], prev[j - 1]});
+            }
+            prev = cur;
+        }
+
+        return prev[n];
+    }
+};
