81f84996c0fc6d319692a785beaa422f2713699c
C P M Ashir <ashircpm@gmail.com>
2026-09-16 18:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (100.00%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    int numberOfSets(int n, int k) {
-        const long long MOD = 1e9 + 7;
-        
-        vector<vector<long long>> dp(n + k, vector<long long>(2 * k + 1));
-        
-        for (int i = 0; i < n + k; i++) {
-            dp[i][0] = 1;
-            for (int j = 1; j <= min(i, 2 * k); j++) {
-                dp[i][j] = (dp[i - 1][j - 1] + 
-                            dp[i - 1][j]) % MOD;
-            }
+    static const long long MOD = 1e9 + 7;
+
+    long long power(long long a, long long b) {
+        long long res = 1;
+        while (b) {
+            if (b & 1) res = res * a % MOD;
+            a = a * a % MOD;
+            b >>= 1;
         }
-        
-        return dp[n + k - 1][2 * k];
+        return res;
+    }
+
+    int numberOfSets(int n, int k) {
+        int N = n + k - 1;
+        int R = 2 * k;
+
+        long long factN = 1, factR = 1, factNR = 1;
+
+        for (int i = 1; i <= N; i++)
+            factN = factN * i % MOD;
+
+        for (int i = 1; i <= R; i++)
+            factR = factR * i % MOD;
+
+        for (int i = 1; i <= N - R; i++)
+            factNR = factNR * i % MOD;
+
+        return factN * power(factR, MOD - 2) % MOD
+                    * power(factNR, MOD - 2) % MOD;
     }
 };
