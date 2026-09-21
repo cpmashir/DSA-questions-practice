663dfa9e5c1cba83e682e0302da28dee9851e33a
C P M Ashir <ashircpm@gmail.com>
2026-09-21 19:21:10 +0530

    Time: 136 ms (83.93%) | Memory: 151.1 MB (55.36%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     vector<long long> resultArray(vector<int>& nums, int k) {
-        vector<long long> ans(k, 0), dp(k, 0);
+        vector<long long> ans(k), dp(k);
 
         for (int num : nums) {
-            vector<long long> ndp(k, 0);
+            vector<long long> ndp(k);
             int x = num % k;
 
-            // Start a new subarray with nums[i]
             ndp[x]++;
 
-            // Extend all subarrays ending at previous index
-            for (int r = 0; r < k; r++) {
-                if (dp[r]) {
-                    ndp[(r * x) % k] += dp[r];
-                }
-            }
+            for (int r = 0; r < k; r++)
+                ndp[(r * x) % k] += dp[r];
 
             dp = ndp;
 
-            // Every subarray ending here contributes to the answer
-            for (int r = 0; r < k; r++) {
+            for (int r = 0; r < k; r++)
                 ans[r] += dp[r];
-            }
         }
 
         return ans;
